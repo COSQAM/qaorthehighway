7a3f99706d9506f3fe2b9cc4154c0a3f800f2d10
Ryan Quellhorst <ryan@deeplens.ai>
2022-10-09 08:23:12 -0400

    added header and speakers

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="background">
+    <div
+      class="text-h4 font-weight-black white--text text-center margin-top"
+    >
+      {{ title }}
+    </div>
+    <div style="height: 30px"></div>
+    <Break class="color" />
+  </div>
+</template>
+
+<script>
+import Break from "@/components/icons/Break";
+
+export default {
+  components: {
+    Break,
+  },
+  props: ["title"],
+};
+</script>
+
+<style scoped>
+.background {
+  background-color: var(--v-primary-base) !important;
+  height: 200px;
+}
+
+.margin-top {
+  padding-top: 80px;
+}
+
+.color {
+  color: white;
+  bottom: none;
+}
+</style>
---
+++ src/pages/Speakers.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-app>
+    <Layout>
+      <v-content>
+        <Header title="Speakers" />
+        <v-row class="text-center max-center mt-5">
+          <v-col
+            cols="12"
+            md="4"
+            class="my-5"
+            v-for="(speaker, index) in combineSpeakers($page.speakers.edges)"
+            :key="index"
+          >
+            <a :href="`./${removeSpaces(speaker.url)}`">
+              <v-avatar size="300">
+                <img :src="findImage(speaker.name)" />
+              </v-avatar>
+            </a>
+            <h5 class="text-h5 pt-3">
+              {{ speaker.name }}
+            </h5>
+          </v-col>
+        </v-row>
+      </v-content>
+    </Layout>
+  </v-app>
+</template>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  components: {
+    Header,
+  },
+  methods: {
+    findImage: (speaker) => {
+      try {
+        return require(`@/assets/images/${speaker.toLowerCase()}.webp`);
+      } catch (error) {
+        return require("@/assets/images/generic-profile.png");
+      }
+    },
+    removeSpaces: function (text) {
+      return text.replace(/\s/g, "");
+    },
+    combineSpeakers: (speakerArray) => {
+      let speakerList = [];
+      speakerArray.map((session) => {
+        speakerList.push({
+          name: session.node.speaker,
+          url: session.node.speaker,
+        });
+        if (session.node.speaker2) {
+          speakerList.push({
+            name: session.node.speaker2,
+            url: session.node.speaker,
+          });
+        }
+      });
+      return speakerList;
+    },
+  },
+};
+</script>
+
+<page-query>
+query currentSessions {
+  speakers: allSession(
+    order: ASC
+  ) {
+    edges {
+      node {
+        speaker
+        speaker2
+        title
+        abstract
+        time
+        room
+      }
+    }
+  },
+}
+</page-query>
+
+<style scoped>
+.max-center {
+  max-width: 1600px;
+  margin: 0 auto;
+  float: none;
+}
+</style>
