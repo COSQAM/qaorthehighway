8529cac4c11ca68d334fc993441a0842a70e41b6
Ryan Quellhorst <ryan@deeplens.ai>
2022-12-17 07:07:54 -0500

    remove g analytics plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,12 +7,5 @@
 module.exports = {
   siteName: 'QA or the Highway',
   siteUrl: 'https://www.qaorthehwy.com',
-  plugins: [
-    {
-      use: '@gridsome/plugin-google-analytics',
-      options: {
-        id: 'G-CR7QVM3F0G'
-      }
-    }
-  ],
+  plugins: [],
 }
