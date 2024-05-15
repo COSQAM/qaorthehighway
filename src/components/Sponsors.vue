c2fefcd08492d1340662b3de641902317b8c47bc
Ryan Quellhorst <ryan@deeplens.ai>
2024-05-15 09:22:10 -0400

    speaker update 05152024

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -168,27 +168,25 @@ export default {
   data() {
     return {
       platinums: [
-        { image: "yourlogo", url: "/contact" },
-        { image: "yourlogo", url: "/contact" },
+        { image: "yourlogo", url: "/contact" }
       ],
       golds: [
         { image: "parasoft", url: "https://www.parasoft.com/" },
         { image: "practitest", url: "https://www.practitest.com/" },
-        { image: "lighthouse", url: "https://lighthousetechnologies.com/" }
+        { image: "lighthouse", url: "https://lighthousetechnologies.com/" },
+        { image: "keysight", url: "https://www.keysight.com/" }
       ],
       silvers: [
         { image: "mabl", url: "https://www.mabl.com/" },
-        { image: "yourlogo", url: "/contact" }
+        { image: "ttc", url: "https://ttcglobal.com/" }
       ],
       lunches: [
         {image: "revel", url: "https://www.revelit.com/"}
       ],
       snacks: [
-        { image: "mabl", url: "https://www.mabl.com/" },
-        { image: "yourlogo", url: "/contact" },
+        { image: "mabl", url: "https://www.mabl.com/" }
       ],
       tshirts: [
-        { image: "yourlogo", url: "/contact" },
         { image: "yourlogo", url: "/contact" }
       ],
     };
