a4a56b2412e2e9389f2fc1453923a5cbd9ad3de1
Ryan Quellhorst <ryan@deeplens.ai>
2025-04-16 09:55:34 -0400

    updates 04162025

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -1,205 +1,80 @@
 <template>
   <v-container class="my-5">
-    <v-row align="center" justify="center"
-      ><h3 class="text-h5 font-weight-black pb-5">SPONSORS</h3></v-row
-    >
-    <v-row align="center" justify="center"
-      ><h3 class="text-h5 font-weight-black primary--text py-5">
-        Platinum Sponsors
-      </h3></v-row
-    >
-    <v-divider></v-divider>
-    <v-row align="center" justify="space-around" class="my-3">
-      <v-col
-        v-for="(platinum, index) in platinums"
-        :key="index"
-        sm="12"
-        md="6"
-        align-self="center"
-        class="d-flex justify-center"
-      >
-        <a :href="`${platinum.url}`" target="_blank">
-          <v-img
-            contain
-            max-height="350"
-            max-width="350"
-            :src="findImage(platinum.image)"
-          />
-        </a>
-      </v-col>
+    <v-row align="center" justify="center">
+      <h3 class="text-h5 font-weight-black pb-5">SPONSORS</h3>
     </v-row>
-
-    <v-row align="center" justify="center"
-      ><h3 class="text-h5 font-weight-black primary--text py-5">
-        Gold Sponsors
-      </h3>
-    </v-row>
-    <v-divider></v-divider>
-    <v-row align-content="center" justify="space-around" class="my-3">
-      <v-col
-        v-for="(gold, index) in golds"
-        :key="index"
-        align-self="center"
-        sm="12"
-        md="6"
-        class="d-flex justify-center"
-      >
-        <a :href="`${gold.url}`" target="_blank">
-          <v-img
-            contain
-            max-height="250"
-            max-width="250"
-            :src="findImage(gold.image)"
-          />
-        </a>
-      </v-col>
-    </v-row>
-    <v-row align="center" justify="center"
-      ><h3 class="text-h5 font-weight-black primary--text py-5">
-        Silver Sponsors
-      </h3>
-    </v-row>
-    <v-divider></v-divider>
-    <v-row align="center" justify="space-around" class="my-3">
-      <v-col
-        v-for="(silver, index) in silvers"
-        :key="index"
-        sm="12"
-        md="4"
-        align-self="center"
-        class="d-flex justify-center"
-      >
-        <a :href="`${silver.url}`" target="_blank">
-          <v-img
-            contain
-            max-height="250"
-            max-width="250"
-            :src="findImage(silver.image)"
-          />
-        </a>
-      </v-col>
-    </v-row>
-        <v-row align="center" justify="center"
-      ><h3 class="text-h5 font-weight-black primary--text py-5">
-        Lunch Sponsors
-      </h3>
-    </v-row>
-    <v-divider></v-divider>
-    <v-row align="center" justify="space-around" class="my-3">
-      <v-col
-        v-for="(lunch, index) in lunches"
-        :key="index"
-        sm="12"
-        md="4"
-        align-self="center"
-        class="d-flex justify-center"
-      >
-        <a :href="`${lunch.url}`" target="_blank">
-          <v-img
-            contain
-            max-height="250"
-            max-width="250"
-            :src="findImage(lunch.image)"
-          />
-        </a>
-      </v-col>
-    </v-row>
-    <v-row align="center" justify="center"
-      ><h3 class="text-h5 font-weight-black primary--text py-5">
-        Snack Sponsors
-      </h3>
-    </v-row>
-    <v-divider></v-divider>
-    <v-row align="center" justify="space-around" class="my-3">
-      <v-col
-        v-for="(snack, index) in snacks"
-        :key="index"
-        sm="12"
-        md="6"
-        align-self="center"
-        class="d-flex justify-center"
-      >
-        <a :href="`${snack.url}`" target="_blank">
-          <v-img
-            contain
-            max-height="250"
-            max-width="250"
-            :src="findImage(snack.image)"
-          />
-        </a>
-      </v-col>
-    </v-row>
-    <v-row align="center" justify="center"
-      ><h3 class="text-h5 font-weight-black primary--text py-5">
-        T-Shirt Sponsors
-      </h3>
-    </v-row>
-    <v-divider></v-divider>
-    <v-row align="center" justify="space-around" class="my-3">
-      <v-col
-        v-for="(tshirt, index) in tshirts"
-        :key="index"
-        sm="12"
-        md="6"
-        align-self="center"
-        class="d-flex justify-center"
-      >
-        <a :href="`${tshirt.url}`" target="_blank">
-          <v-img
-            contain
-            max-height="250"
-            max-width="250"
-            :src="findImage(tshirt.image)"
-          />
-        </a>
-      </v-col>
-    </v-row>
+    <SponsorSection
+      v-for="(section, index) in sponsorSections"
+      :key="index"
+      :title="section.title"
+      :sponsors="section.sponsors"
+      :maxHeight="section.maxHeight"
+      :maxWidth="section.maxWidth"
+    />
     <Break class="color" />
   </v-container>
 </template>
 
 <script>
 import Break from "@/components/icons/Break";
+import SponsorSection from "@/components/SponsorSection";
 
 export default {
   components: {
     Break,
+    SponsorSection,
   },
   data() {
     return {
-      platinums: [
-        { image: "ttc", url: "https://ttcglobal.com/" }
-      ],
-      golds: [
-        { image: "lighthouse", url: "https://lighthousetechnologies.com/" },
-        { image: "tricentis", url: "https://www.tricentis.com/" }
-      ],
-      silvers: [
-        { image: "yourlogo", url: "/contact" },
-        { image: "yourlogo", url: "/contact" }
-      ],
-      lunches: [
-        {image: "yourlogo", url: "/contact"}
-      ],
-      snacks: [
-        { image: "yourlogo", url: "/contact" }
-      ],
-      tshirts: [
-        { image: "heritagedental", url: "https://www.heritagedentalohio.com/" }
+      sponsorSections: [
+        {
+          title: "Platinum Sponsors",
+          sponsors: [{ image: "ttc", url: "https://ttcglobal.com/" }],
+          maxHeight: 350,
+          maxWidth: 350,
+        },
+        {
+          title: "Gold Sponsors",
+          sponsors: [
+            { image: "lighthouse", url: "https://lighthousetechnologies.com/" },
+            { image: "tricentis", url: "https://www.tricentis.com/" },
+            { image: "narwal", url: "https://narwal.ai/" },
+          ],
+          maxHeight: 250,
+          maxWidth: 250,
+        },
+        {
+          title: "Silver Sponsors",
+          sponsors: [
+            { image: "yourlogo", url: "/contact" },
+            { image: "yourlogo", url: "/contact" },
+          ],
+          maxHeight: 250,
+          maxWidth: 250,
+        },
+        {
+          title: "Lunch Sponsors",
+          sponsors: [{ image: "yourlogo", url: "/contact" }],
+          maxHeight: 250,
+          maxWidth: 250,
+        },
+        {
+          title: "Snack Sponsors",
+          sponsors: [{ image: "yourlogo", url: "/contact" }],
+          maxHeight: 250,
+          maxWidth: 250,
+        },
+        {
+          title: "T-Shirt Sponsors",
+          sponsors: [
+            { image: "heritagedental", url: "https://www.heritagedentalohio.com/" },
+            { image: "narwal", url: "https://narwal.ai/" },
+          ],
+          maxHeight: 250,
+          maxWidth: 250,
+        },
       ],
     };
-  },
-  methods: {
-    findImage: (sponsor) => {
-      try {
-        return require(`@/assets/images/sponsors/${sponsor}.webp`);
-      } catch (error) {
-        return require("@/assets/images/generic-profile.png");
-      }
-    },
-    removeSpaces: function(text) {
-      return text.replace(/\s/g, "");
-    },
   },
 };
 </script>
